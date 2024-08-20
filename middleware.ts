c6f568e438936ab5a09206bbe1b95698c2f930ad
Mark Lyck <MarkLyck@users.noreply.github.com>
2024-08-20 15:20:15 -0400

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,29 @@
-export { auth as middleware } from "auth"
+import { auth } from '@/auth'
 
-// Or like this if you need to do something here.
-// export default auth((req) => {
-//   console.log(req.auth) //  { session: { user: { ... } } }
-// })
+export default auth(async (req) => {
+  // If user is not authenticated, redirect to signin page
+  if (
+    !req.auth &&
+    req.nextUrl.pathname !== '/auth/login' &&
+    req.nextUrl.pathname !== '/api/auth/force-signin'
+  ) {
+    return Response.redirect(new URL('/api/auth/force-signin', req.url))
+  }
+})
 
 // Read more: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - icons (public folder)
+     * - images (public folder)
+     * - logos (public folder)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico|icons|images|logos).*)',
+  ],
 }
